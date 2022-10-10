4320ee8f0b54eac7a8810f7e87e6d43e3bc7eca2
wingmeter <nursb189@gmail.com>
2022-10-10 23:36:54 +0600

    docs: final commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Companies from './components/Companies';
+import Guide from './components/Guide';
+import Hero from './components/Hero';
+import Properties from './components/Properties';
+import Details from './components/Details';
+import GetStarted from './components/GetStarted';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Companies />
+      <Guide />
+      <Properties />
+      <Details />
+      <GetStarted />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,71 @@
+import { Box, styled, Typography } from '@mui/material';
+import { Container } from '@mui/system';
+import React from 'react';
+import Navbar from './Navbar';
+
+import heroImg from '../assets//hero_illustration.png';
+import CustomButton from './CustomButton';
+
+const Hero = () => {
+  const CustomBox = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    justifyContent: 'center',
+    gap: theme.spacing(5),
+    marginTop: theme.spacing(3),
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column',
+      alignItems: 'center',
+      textAlign: 'center',
+    },
+  }));
+
+  const Title = styled(Typography)(({ theme }) => ({
+    fontSize: '64px',
+    color: '#000336',
+    fontWeight: 'bold',
+    margin: theme.spacing(4, 0, 4, 0),
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '40px',
+    },
+  }));
+
+  return (
+    <Box sx={{ backgroundColor: '#e6f0ff', minHeight: '80vh' }}>
+      <Container>
+        <Navbar />
+        <CustomBox>
+          <Box sx={{ flex: '1' }}>
+            <Typography
+              variant="body2"
+              sx={{ fontSize: '18px', color: '#697690', mt: 10, mb: 4 }}
+            >
+              Welcome to Besnik Agency
+            </Typography>
+            <Title variant="h1">
+              Discover a place where you'll love to live.
+            </Title>
+            <Typography sx={{ fontSize: '18px', color: '#5a6473', my: 4 }}>
+              Be the first to get the best real estate deals before they hit the
+              mass market! Hot foreclosure deals with one simple search!
+            </Typography>
+            <CustomButton
+              backgroundColor="#0f1b4c"
+              color="#fff"
+              buttonText="More About Us"
+              heroBtn={true}
+            />
+          </Box>
+          <Box sx={{ flex: '1.25' }}>
+            <img
+              src={heroImg}
+              alt="heroImg"
+              style={{ maxWidth: '100%', marginBottom: '2rem' }}
+            />
+          </Box>
+        </CustomBox>
+      </Container>
+    </Box>
+  );
+};
+
+export default Hero;
